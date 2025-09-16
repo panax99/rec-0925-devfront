c52187e8dcafa5ec7480b2c4c3405f59096bb89d
panax99 <panarandri@gmail.com>
2025-09-16 09:34:54 +0300

    feat: add custom Button Blade component

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<div class="rounded-md bg-[#BF2A6B] flex items-center justify-center gap-2 text-white text-center px-10 py-2">
+    <p>{{$slot}}</p>
+    <i class="fa-solid fa-arrow-right text-white mt-1"></i>
+</div>
