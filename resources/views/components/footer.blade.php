0f989936ac575e24b67ef2f830e8e8c9cb4ec677
panax99 <panarandri@gmail.com>
2025-09-16 09:23:18 +0300

    feat: add navbar and footer components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,23 @@
+<div class="w-full px-20 md:fixed bottom-0 flex items-center justify-between py-5 bg-[#353535]">
+    <div class="bg-[#F2F3F5] flex items-center gap-2 p-3 px-5 rounded-xl border border-[#70707040]">
+        <i class="fa-regular fa-calendar-check text-[#BF2A6B] fill-transparent"></i>
+        <div class="flex flex-col text-[#36578A]">
+            <p class="text-base font-semibold">
+                Test psychotechnique le 28 mars à 14:00
+            </p>
+            <p class="text-xs">
+                15 rue de la république Montgeron 91230
+            </p>
+        </div>
+    </div>
+    <div class="flex flex-col justify-between gap-2">
+        <div class="flex items-center justify-between w-auto text-white">
+            <p>Total</p>
+            <p class="text-3xl font-semibold">135 €</p>
+        </div>
+        <x-button>
+            <span>Suivant</span> 
+            <i></i>
+        </x-button>
+    </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<div class="w-full flex flex-col items-center justify-center">
+    <div class="w-full px-20 py-5 mb-2">
+        <img src="images/logo.png" class="bg-cover object-cover" alt="Logo">
+    </div>
+    <div class="w-full px-20 py-6 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_4px_6px_-1px_rgba(0,0,0,0.1)]">
+        <p class="text-lg font-semibold text-[#36578A]">
+            Test psychotechnique pour permis de conduire à Montgeron
+        </p>
+    </div>
+</div>
